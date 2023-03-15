0e5b772283836712271597d742946fb91120f5e0
sod-efi <127058935+sod-efi@users.noreply.github.com>
2023-03-15 00:50:23 -0700

    Update app.py
    adding a signet button

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ def authenticate(username, password):
         return True
     return False
 
+def sign_out():
+    if st.button('Sign Out'):
+        st.session_state.authenticated = False
+
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
