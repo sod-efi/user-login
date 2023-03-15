2fdb4af4ffadc3d04f44bdd5b2074c9ed29b0fc6
sod-efi <127058935+sod-efi@users.noreply.github.com>
2023-03-15 00:54:43 -0700

    Update app.py
    main() function was not defined. I'll move the app content into a main_app() function and make the necessary adjustments.

--- app.py
+++ app.py
@@ -4,9 +4,7 @@ from collections import namedtuple
 import altair as alt
 import math
 
-
 def authenticate(username, password):
-    # Replace the following lines with your own authentication logic
     valid_usernames = ['user1', 'user2']
     valid_passwords = ['password1', 'password2']
 
@@ -18,33 +16,35 @@ def sign_out():
     if st.button('Sign Out'):
         st.session_state.authenticated = False
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def main_app():
+    with st.echo(code_location='below'):
+        total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+        num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+        Point = namedtuple('Point', 'x y')
+        data = []
 
-    points_per_turn = total_points / num_turns
+        points_per_turn = total_points / num_turns
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+        for curr_point_num in range(total_points):
+            curr_turn, i = divmod(curr_point_num, points_per_turn)
+            angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+            radius = curr_point_num / total_points
+            x = radius * math.cos(angle)
+            y = radius * math.sin(angle)
+            data.append(Point(x, y))
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+        st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+            .mark_circle(color='#0068c9', opacity=0.5)
+            .encode(x='x:Q', y='y:Q'))
     
+    sign_out()
 
 if 'authenticated' not in st.session_state:
     st.session_state.authenticated = False
 
 if st.session_state.authenticated:
-    main()
+    main_app()
 else:
     st.title('Login')
     username = st.text_input('Username')
@@ -53,9 +53,6 @@ else:
     if st.button('Login'):
         if authenticate(username, password):
             st.session_state.authenticated = True
-            main()
+            main_app()
         else:
             st.error('Invalid username or password')
-            
-
-
